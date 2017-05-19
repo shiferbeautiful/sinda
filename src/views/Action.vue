26355d636867dfda24ee1a88c4431d212a928d90
胡文飞 <2654hott@163.com>
2017-05-19 14:13:16 +0800

    添加了action的头

--- src/views/Action.vue
+++ src/views/Action.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
-    <!--<a-head></a-head>-->
+    <a-head></a-head>
     <router-view></router-view>
     <!--<a-foot></a-foot>-->
   </div>
 </template>
 
 <script>
+import aHead from '../components/ahead'
 export default {
   name: 'hello',
   data() {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components:{
+    aHead
   }
 }
 
