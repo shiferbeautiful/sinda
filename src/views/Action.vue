e96563a8ea28d31b6132b3f5fa9f1ca5d5660963
胡文飞 <2654hott@163.com>
2017-05-19 13:59:14 +0800

    初始化工程

---
+++ src/views/Action.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!--<a-head></a-head>-->
+    <router-view></router-view>
+    <!--<a-foot></a-foot>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
